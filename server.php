a260cf2fb91e8708db079c2760370de2fab9c42d
João Pedro <aspuma13@gmail.com>
2023-12-05 11:22:42 -0300

    Add files via upload

---
+++ server.php
@@ -0,0 +1,12 @@
+<?php
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Process form data
+
+    // Example: Get form data
+    $name = $_POST["name"];
+    $message = "Nova submissão de $name";
+
+    //
+}
+?>
